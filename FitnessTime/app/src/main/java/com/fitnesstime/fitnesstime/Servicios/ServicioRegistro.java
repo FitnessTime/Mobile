ea364cf747f662b537f60250401a5de09efb7d38
skalic.julian <skalic.julian@gmail.com>
2016-04-27 22:21:29 -0300

    .

--- FitnessTime/app/src/main/java/com/fitnesstime/fitnesstime/Servicios/ServicioRegistro.java
+++ FitnessTime/app/src/main/java/com/fitnesstime/fitnesstime/Servicios/ServicioRegistro.java
@@ -6,6 +6,8 @@ import com.fitnesstime.fitnesstime.Modelo.SecurityToken;
 import com.fitnesstime.fitnesstime.ModelosFlujo.Registro;
 import com.google.gson.Gson;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -22,9 +24,19 @@ public class ServicioRegistro {
             Gson gson = new Gson();
             URL url = new URL("http://api-fitnesstime.herokuapp.com/registrarUsuario?usuario=" + gson.toJson(registro, Registro.class));
             HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("PUT");
             urlConnection.setConnectTimeout(3000);
+            urlConnection.setDoInput(true);
+            urlConnection.setDoOutput(true);
             code = urlConnection.getResponseCode();
+            BufferedReader br=new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+            String response = "";
+            String line="";
+            while ((line=br.readLine()) != null) {
+                response+=line;
+            }
             code = code!=200? 500 : 200;
+
         }catch(Exception e)
         {
             Log.println(1, "", e.getMessage());
